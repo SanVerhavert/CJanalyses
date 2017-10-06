8d06a7afdf9a33cf9ae3c03f3de2ca6ff11a2920
SanVerhavert <san.verhavert@uantwerpen.be>
2017-10-06 13:23:37 +0200

    Added bare structure for full analysis

---
+++ CJanalyses/R/btlanalysis.b.R
@@ -0,0 +1,15 @@
+
+# This file is a generated template, your changes will not be overwritten
+
+BTLanalysisClass <- if (requireNamespace('jmvcore')) R6::R6Class(
+    "BTLanalysisClass",
+    inherit = BTLanalysisBase,
+    private = list(
+        .run = function() {
+
+            # `self$data` contains the data
+            # `self$options` contains the options
+            # `self$results` contains the results object (to populate)
+
+        })
+)
